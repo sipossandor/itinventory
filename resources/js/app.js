a7c9ffb916acd54df2bf5904ce87797746418e20
sipossandor <alexsipos13@yahoo.com>
2022-06-09 21:38:29 +0200

    M2 project, V10.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,14 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs';
 
+import {createApp} from "vue";
+import VueAxios from "vue-axios";
+import axios from "axios";
+import App from './vue.js';
+const app = createApp({
+
+});
+
 window.Alpine = Alpine;
 
 Alpine.start();
