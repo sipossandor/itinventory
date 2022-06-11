eb59dd5bfeed27bd8ee2e318ac2e6cb374876719
sipossandor <alexsipos13@yahoo.com>
2022-06-11 21:46:10 +0200

    M2 project, V13.

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <br>
+    <hr>
+    Hello VueJS!
+    <hr>
+    <CounterButton :startv="11" :min="1" :max="13"></CounterButton>
+    <br>
+    <counter-button></counter-button>
+    <hr>
+    <hr>
+    <TemplateBox></TemplateBox>
+</template>
+
+<script>
+import CounterButton from "./components/CounterButton";
+import TemplateBox from "./components/TemplateBox";
+
+export default {
+    name: "App",
+    components: {
+        CounterButton,
+        TemplateBox
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
